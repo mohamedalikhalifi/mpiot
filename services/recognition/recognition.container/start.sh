363c49fb1b7dca47bb57618d940b6609747553df
Mohamed Ali Hhalifi <mohamedali.khalifi@gmail.com>
2020-12-13 21:25:44 +0100

    introduce shared datastore

--- services/recognition/recognition.container/start.sh
+++ services/recognition/recognition.container/start.sh
@@ -4,6 +4,6 @@ sudo docker run -it \
     --device=/dev/vchiq \
     --device=/dev/video0 \
     --mount type=bind,source="$(pwd)/../service",target=/service \
-    --mount type=bind,source="$(pwd)/../shared",target=/shared \
+    --mount type=bind,source="$(pwd)/../../shared",target=/shared \
     ${PWD##*/} \
     /bin/bash
