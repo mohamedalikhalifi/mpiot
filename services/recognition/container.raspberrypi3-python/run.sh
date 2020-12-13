59dbd06722af4be81966557df00ab4613756431d
Mohamed Ali Hhalifi <mohamedali.khalifi@gmail.com>
2020-12-13 13:47:05 +0100

    add deployment

--- services/recognition/container.raspberrypi3-python/run.sh
+++ services/recognition/container.raspberrypi3-python/run.sh
@@ -1,5 +1,5 @@
 echo "**************************BUILDING CONTAINER****************************#"
-sudo docker image build -t test .
+sudo docker image build -t test2 .
 
 echo "**************************RUNNING CONTAINER*****************************#"
-sudo docker run -it -p 8000:8000 --device=/dev/vcsm  --device=/dev/vchiq --device=/dev/video0 --mount type=bind,source="$(pwd)",target=/service test /bin/bash
+sudo docker run -it -p 8000:8000 --device=/dev/vcsm  --device=/dev/vchiq --device=/dev/video0 --mount type=bind,source="$(pwd)/../service",target=/service $(pwd) /bin/bash
