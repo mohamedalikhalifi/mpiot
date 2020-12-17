6e9a1ddf7a37f23b44d9bd5b22aa919aa36fdda6
Mohamed Ali Hhalifi <mohamedali.khalifi@gmail.com>
2020-12-17 22:26:27 +0100

    entrypoint after mount

--- services/recognition/recognition_container/entrypoint.sh
+++ services/recognition/recognition_container/entrypoint.sh
@@ -1,2 +1,8 @@
+while [ ! -d '/service' ]; do
+    sleep 10
+done
+
 cd /service
+echo "staring  recogintion service"
 python recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
+
