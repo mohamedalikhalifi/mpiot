517fc2c1c2b3bc30c2d182c76e5e57a8694e153f
Mohamed Ali Khalifi <mohamedali.khalifi@gmail.com>
2020-12-20 21:53:41 +0100

    implementing mqtt publishing service

--- services/publishing/service/publishing.py
+++ services/publishing/service/publishing.py
@@ -3,18 +3,22 @@ import time
 import paho.mqtt.client  as mqtt
 import redis
 
-
+#connect to mqtt broker
 mqttClient = mqtt.Client()
-messenger = redis.Redis(host=os.environ['MESSENGER_HOST'], port=os.environ['MESSENGER_PORT'])
 mqttClient.Connect(os.environ['MQTT_BROKER'], os.environ['MQTT_PORT'], 60)
 
-i = 0
-while True:
- time.sleep(5)
- i+=1
- message = str(i)
- if i <=10:
-  mqttClient.publish(topic, message)
- else:
-  break
-client.disconnect()
+#connect to message bus
+messenger = redis.Redis(host=os.environ['MESSENGER_HOST'], port=os.environ['MESSENGER_PORT'])
+messageBus = messenger.pubsub()
+messageBus.subscribe("AccessRequest")
+oldRequester = "Unknown"
+sessionRequestCounter = 0
+for message in messageBus.listen():
+    sleep(0.1)
+    newRequester = message["data"]
+    if (newRequester != oldRequester)
+          mqttClient.publish("AccessRequested", newRequester)
+          mqttClient.publish("SessionAccessRequestCount", ++sessionRequestCounter)
+          oldRequester = newRequester
+
+mqttClient.disconnect()
