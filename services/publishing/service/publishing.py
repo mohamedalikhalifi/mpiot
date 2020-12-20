0b6315127f00be251b190f759e5bf99d43c989e9
Mohamed Ali Hhalifi <mohamedali.khalifi@gmail.com>
2020-12-20 11:58:11 +0100

    implementing publisher service

--- services/publishing/service/publishing.py
+++ services/publishing/service/publishing.py
@@ -1,12 +1,12 @@
 import os
 import time
-import paho.mqtt.client as mqtt
+import paho.mqtt.clientgit  as mqtt
 import redis
 
 
 mqttClient = mqtt.Client()
 messenger = redis.Redis(host=os.environ['MESSENGER_HOST'], port=os.environ['MESSENGER_PORT'])
-mqttClient.Connect(os.environ['MESSENGER_HOST'], os.environ['MESSENGER_PORT'], keepalive)
+mqttClient.Connect(os.environ['MQTT_BROKER'], os.environ['MQTT_PORT'], keepalive)
 
 i = 0
 while True:
