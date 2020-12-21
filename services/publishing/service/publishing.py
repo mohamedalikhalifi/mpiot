ce3501d086b77728b33b17d5d24f5f971dee86e4
Mohamed Ali Hhalifi <mohamedali.khalifi@gmail.com>
2020-12-21 02:17:41 +0100

    implemented and tested MQTT publication of Access requests

--- services/publishing/service/publishing.py
+++ services/publishing/service/publishing.py
@@ -1,24 +1,34 @@
 import os
-import time
+from time import sleep
 import paho.mqtt.client  as mqtt
 import redis
 
 #connect to mqtt broker
+print("start")
 mqttClient = mqtt.Client()
-mqttClient.Connect(os.environ['MQTT_BROKER'], os.environ['MQTT_PORT'], 60)
+broker = os.environ['MQTT_BROKER']
+port = int(os.environ['MQTT_PORT'])
+print("Connecting to " + broker + ":" + str(port))
+mqttClient.connect(broker, port, 60)
+print("Connected to " + broker + ":" + str(port))
 
 #connect to message bus
-messenger = redis.Redis(host=os.environ['MESSENGER_HOST'], port=os.environ['MESSENGER_PORT'])
+redisHost = os.environ['MESSENGER_HOST']
+redisPort = os.environ['MESSENGER_PORT']
+print(" connecting to " + redisHost +":"+str(redisPort))
+messenger = redis.Redis(host=redisHost, port=redisPort)
 messageBus = messenger.pubsub()
 messageBus.subscribe("AccessRequest")
+
+# send MQTT publication on new access request
 oldRequester = "Unknown"
 sessionRequestCounter = 0
 for message in messageBus.listen():
-    sleep(0.1)
     newRequester = message["data"]
-    if (newRequester != oldRequester)
-          mqttClient.publish("AccessRequested", newRequester)
-          mqttClient.publish("SessionAccessRequestCount", ++sessionRequestCounter)
-          oldRequester = newRequester
-
+    if (newRequester != oldRequester):
+        sessionRequestCounter += 1
+        print("AccessRequest from: " + str(newRequester) )
+        mqttClient.publish("AccessRequested", newRequester)
+        mqttClient.publish("SessionAccessRequestCount", str(sessionRequestCounter))
+        oldRequester = newRequester
 mqttClient.disconnect()
