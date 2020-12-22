c51b05c437334e00b4c0e44a64ffab039d90c83d
Mohamed Ali Khalifi <mohamedali.khalifi@gmail.com>
2020-12-22 03:22:05 +0100

    implemented watchdog to prevent stuck on unknown face

--- services/publishing/service/publishing.py
+++ services/publishing/service/publishing.py
@@ -2,33 +2,64 @@ import os
 from time import sleep
 import paho.mqtt.client  as mqtt
 import redis
+import services.publishing.service.watchdog as Watchdog
 
+watchdog = None
+mqttClient = None
+messageBus = None
+requester = None
+old_requester = ""
 #connect to mqtt broker
-print("start")
-mqttClient = mqtt.Client()
-broker = os.environ['MQTT_BROKER']
-port = int(os.environ['MQTT_PORT'])
-print("Connecting to " + broker + ":" + str(port))
-mqttClient.connect(broker, port, 60)
-print("Connected to " + broker + ":" + str(port))
+def init_publishing(): 
+    global mqttClient
+    global messageBus
+    print("start")
+    mqttClient = mqtt.Client()
+    broker = os.environ['MQTT_BROKER']
+    port = int(os.environ['MQTT_PORT'])
+    print("Connecting to " + broker + ":" + str(port))
+    mqttClient.connect(broker, port, 60)
+    print("Connected to " + broker + ":" + str(port))
 
-#connect to message bus
-redisHost = os.environ['MESSENGER_HOST']
-redisPort = os.environ['MESSENGER_PORT']
-print(" connecting to " + redisHost +":"+str(redisPort))
-messenger = redis.Redis(host=redisHost, port=redisPort)
-messageBus = messenger.pubsub()
-messageBus.subscribe("AccessRequest")
+    #connect to message bus
+    redisHost = os.environ['MESSENGER_HOST']
+    redisPort = os.environ['MESSENGER_PORT']
+    print(" connecting to " + redisHost +":"+str(redisPort))
+    messenger = redis.Redis(host=redisHost, port=redisPort)
+    messageBus = messenger.pubsub()
+    messageBus.subscribe("AccessRequest")
 
-# send MQTT publication on new access request
-oldRequester = "Unknown"
-sessionRequestCounter = 0
-for message in messageBus.listen():
-    newRequester = message["data"]
-    if (newRequester != oldRequester):
-        sessionRequestCounter += 1
-        print("AccessRequest from: " + str(newRequester) )
-        mqttClient.publish("AccessRequested", newRequester)
-        mqttClient.publish("SessionAccessRequestCount", str(sessionRequestCounter))
-        oldRequester = newRequester
-mqttClient.disconnect()
+def flush_old_requester():
+    global old_requester
+    global watchdog
+    old_requester = ""
+    watchdog.reset()
+
+def listen_and_foreward():
+    global old_requester
+    global mqttClient
+    global messageBus
+    global watchdog
+    # launch watchdog timer to flush the old requester buffer on inactivity
+    watchdog = Watchdog(20, flush_old_requester)
+
+    # send MQTT publication on new access request
+    sessionRequestCounter = 0
+    for message in messageBus.listen():
+        newRequester = message["data"]
+        # exclude empty message
+        if (newRequester == '1' ):
+            continue
+        # Notify on new Access request
+        if (old_requester != newRequester):
+            sessionRequestCounter += 1
+            mqttClient.publish("AccessRequested", newRequester)
+            mqttClient.publish("SessionAccessRequestCount", str(sessionRequestCounter))
+            old_requester = newRequester
+    watchdog.reset()
+
+try:
+    init_publishing()
+    listen_and_foreward()
+finally:
+    mqttClient.disconnect()
